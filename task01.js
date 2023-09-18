d0005519f043a5cbffe44a0315e4eba3f9398d3a
tegza00 <139542222+tegza00@users.noreply.github.com>
2023-09-19 00:23:12 +0300

    Add files via upload

---
+++ task01.js
@@ -0,0 +1,19 @@
+function Account({ login, email }) {
+    this.login = login;
+    this.email = email;
+  }
+  Account.prototype.getInfo = function() {
+    console.log(`Login: ${this.login}, Email: ${this.email}`);
+  };
+  console.log(typeof Account.prototype.getInfo); 
+  const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+  });
+  mango.getInfo();
+  const poly = new Account({
+    login: 'Poly',
+    email: 'poly@mail.com',
+  });
+  poly.getInfo(); 
+  
---
+++ task04.js
@@ -0,0 +1,26 @@
+class StringBuilder {
+    constructor(initialValue) {
+      this._value = initialValue;
+    }
+    get value() {
+      return this._value;
+    }
+    append(str) {
+      this._value += str;
+    }
+    prepend(str) {
+      this._value = str + this._value;
+    }
+    pad(str) {
+      this.prepend(str);
+      this.append(str);
+    }
+  }
+  const builder = new StringBuilder('.');
+  builder.append('^');
+  console.log(builder.value); 
+  builder.prepend('^');
+  console.log(builder.value); 
+  builder.pad('=');
+  console.log(builder.value); 
+  
